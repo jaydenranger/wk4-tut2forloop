5862c637b08dbaacdfe65e95d511c35687a5831c
Jayden Ranger <10012125@BCS.NET.NZ>
2017-08-03 16:27:10 +1200

    tut2for

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace tut2for
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Console.WriteLine("Give me a number");
+            var input1 = Console.ReadLine();
+            var number1 = 0;
+            var isNumber = int.TryParse(input1, out number1);
+          
+           
+           for( var Count = 0; Count < 12; Count++)
+           {
+                var a = Count + 1;
+                Console.WriteLine(number1 + " Times " + a + " = "+ number1*a);
+                
+           }
+        }
+    }
+}
